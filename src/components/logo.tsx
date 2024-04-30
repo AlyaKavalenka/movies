b5ae5057cd4c5e2d6d452c739dbca24087088f93
Alya Kavalenka <kovalenkoalinam@gmail.com>
2024-04-30 20:54:34 +0300

    fix: eslint and prettier problems

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -7,8 +7,20 @@ const poppins = Poppins({ weight: '600', subsets: ['latin'] });
 export default function Logo() {
   return (
     <Flex gap="sm" align="center" justify="start">
-      <Image src={logoSvg} alt="ArrowFlicks icon" priority width={32} height={32} />
-      <Title c="purple.5" style={{ ...poppins.style, letterSpacing: '-0.02em' }} size="24px">ArrowFlicks</Title>
+      <Image
+        src={logoSvg}
+        alt="ArrowFlicks icon"
+        priority
+        width={32}
+        height={32}
+      />
+      <Title
+        c="purple.5"
+        style={{ ...poppins.style, letterSpacing: '-0.02em' }}
+        size="24px"
+      >
+        ArrowFlicks
+      </Title>
     </Flex>
   );
 }
